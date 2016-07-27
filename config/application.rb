2ebd7ebb2d5f548ea3a74d326279f4867014836a
tonklis <benjamin.hernandez@in2teck.com>
2016-07-27 16:24:57 -0500

    Added timezone.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module LavaderoBackend
   class Application < Rails::Application
+
+    config.time_zone = 'Mexico City'    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
